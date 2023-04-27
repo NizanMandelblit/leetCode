764187f62b72c9fdeaf471f2273483f7fce83438
Nizan Mandelblit <nizanm91@gmail.com>
2023-04-27 16:59:47 +0300

    addTwoNumbers.c

--- AddTwoNumbers.c
+++ AddTwoNumbers.c
@@ -18,7 +18,6 @@
 #include <stdlib.h>
 #include <math.h>
 
-
 struct ListNode
 {
     int val;
@@ -30,66 +29,78 @@ struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
     int l1Sum = 0;
     int l2Sum = 0;
     int l3Sum = 0;
-
+    double pos = 0;
     struct ListNode *ptr = l1;
-    int i=0;
+    int i = 0;
     while (ptr != NULL)
     {
-        l1Sum =l1Sum + (ptr->val*(int)pow(10,i));
+        pos = pow(10, i);
+        l1Sum = l1Sum + (ptr->val * (pos));
         ptr = ptr->next;
         i++;
     }
     ptr = l2;
 
-i=0;
+    i = 0;
     while (ptr != NULL)
     {
-        l2Sum =l2Sum+ (ptr->val*pow(10,i));
+        pos = pow(10, i);
+        l2Sum = l2Sum + (ptr->val * (pos));
         ptr = ptr->next;
         i++;
     }
 
     l3Sum = l1Sum + l2Sum;
+    int l3SumRev = 0;
+    i = 0;
+    while (l3Sum)
+    {
+        pos = pow(10, i);
+        l3SumRev = l3SumRev + ((l3Sum % 10) * (pos));
+        l3Sum /= 10;
+        i++;
+    }
 
     struct ListNode *l3 = (struct ListNode *)malloc(1 * sizeof(struct ListNode));
     l3->next = NULL;
     l3->val = 0;
     ptr = l3;
-    while (l3Sum)
+    while (l3SumRev)
     {
-        l3->val = l3Sum % 10;
-        l3Sum /= 10;
-        if (l3Sum)
+        ptr->val = l3SumRev % 10;
+        l3SumRev /= 10;
+        if (l3SumRev)
         {
             ptr->next = (struct ListNode *)malloc(1 * sizeof(struct ListNode));
+            ptr = ptr->next;
+        }
+        else
+        {
+            ptr->next = NULL;
         }
     }
-    ptr->next=NULL;
 
     return l3;
 }
 
+void main()
+{
 
+    struct ListNode *l1 = (struct ListNode *)malloc(1 * sizeof(struct ListNode));
+    l1->val = 2;
+    l1->next = (struct ListNode *)malloc(1 * sizeof(struct ListNode));
+    l1->next->val = 4;
+    l1->next->next = (struct ListNode *)malloc(1 * sizeof(struct ListNode));
+    l1->next->next->val = 3;
+    l1->next->next->next = NULL;
 
-void main(){
+    struct ListNode *l2 = (struct ListNode *)malloc(1 * sizeof(struct ListNode));
+    l2->val = 5;
+    l2->next = (struct ListNode *)malloc(1 * sizeof(struct ListNode));
+    l2->next->val = 6;
+    l2->next->next = (struct ListNode *)malloc(1 * sizeof(struct ListNode));
+    l2->next->next->val = 4;
+    l2->next->next->next = NULL;
 
-struct ListNode *l1=(struct ListNode *) malloc(1*sizeof(struct ListNode ));
-l1->val=2;
-l1->next=(struct ListNode *) malloc(1*sizeof(struct ListNode ));
-l1->next->val=4;
-l1->next->next=(struct ListNode *) malloc(1*sizeof(struct ListNode ));
-l1->next->next->val=3;
-l1->next->next->next=NULL;
-
-struct ListNode *l2=(struct ListNode *) malloc(1*sizeof(struct ListNode ));
-l2->val=5;
-l2->next=(struct ListNode *) malloc(1*sizeof(struct ListNode ));
-l2->next->val=6;
-l2->next->next=(struct ListNode *) malloc(1*sizeof(struct ListNode ));
-l2->next->next->val=4;
-l2->next->next->next=NULL;
-
-
-
-addTwoNumbers(l1, l2);
+    addTwoNumbers(l1, l2);
 }
